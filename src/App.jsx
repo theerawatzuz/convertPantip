e21869923dd8f3beb5ed4e92272f7b78f5f60145
silimechok <theerawat.silimechok@accenture.com>
2024-03-05 16:06:59 +0700

    createNavbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 
 import Navbar from './Nav'
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
 
 function App() {
 
@@ -8,7 +11,10 @@ function App() {
     <>
       
         <Navbar/>
-       
+        <Container maxWidth="lg">
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', mt: 2 }} />
+        ไทย
+      </Container>
     </>
   )
 }
